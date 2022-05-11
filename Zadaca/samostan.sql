0ed831194d8bf7cbd0bf5ac5c4f026ac3380b55e
NFilip0 <filip.nad300@gmail.com>
2022-05-11 21:08:00 +0200

    update,alter,izmjena ER diagrama

--- Zadaca/samostan.sql
+++ Zadaca/samostan.sql
@@ -13,19 +13,24 @@ create database samostan;
 use samostan;
 
 create table svecenik(
-    sifra varchar(50),
-    ime varchar(50),
-    prezime varchar(50),
-    nadredeni_svecenik varchar(50)
+    sifra int not null primary key auto_increment,
+    ime varchar(50) not null,
+    prezime varchar(50) not null,
+    nadredeni_svecenik int not null
 );
 
 create table posao(
-    sifra varchar(50),
-    naziv varchar(50),
+    sifra int not null primary key auto_increment,
+    naziv varchar(50) not null,
     trajanje varchar(50)
 );
 
 create table svecenik_posao(
-    svecenik varchar(50),
-    posao varchar(50)
+    svecenik int not null,
+    posao int not null
 );
+
+alter table svecenik add foreign key (nadredeni_svecenik) references svecenik(sifra);
+
+alter table svecenik_posao add foreign key (svecenik) references svecenik(sifra);
+alter table svecenik_posao add foreign key (posao) references posao(sifra);
