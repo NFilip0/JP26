0d729f0a48fb6627c342c0c1a4d99daf7c014d65
NFilip0 <filip.nad300@gmail.com>
2022-05-13 22:34:58 +0200

    izmjena tipova podataka, alter table

--- Zadaca/doktorska_ordinacija.sql
+++ Zadaca/doktorska_ordinacija.sql
@@ -11,43 +11,48 @@ create database doktorska_ordinacija;
 use doktorska_ordinacija;
 
 create table osoba(
-    sifra varchar(50),
-    ime varchar(50),
-    prezime varchar(50),
-    oib varchar(50)
+    sifra int not null primary key auto_increment,
+    ime varchar(50) not null,
+    prezime varchar(50) not null,
+    oib char(11) not null
 );
 
 create table doktor(
-    sifra varchar(50),
-    osoba varchar(50),
+    sifra int not null primary key auto_increment,
+    osoba int not null,
     iban varchar(50)
 );
 
 create table medicinska_sestra(
-    sifra varchar(50),
-    osoba varchar(50),
+    sifra int not null primary key auto_increment,
+    osoba int not null,
     iban varchar(50)
 );
 
-create table pacijent(
-    sifra varchar(50),
-    osoba varchar(50)
-);
-
 create table lijecenje(
-    sifra varchar(50),
-    doktor varchar(50),
-    trajanje varchar(50)
+    sifra int not null primary key auto_increment,
+    doktor int not null,
+    trajanje boolean
 );
 
 create table lijecenje_sestra(
-    sifra varchar(50),
-    lijecenje varchar(50),
-    medicinska_sestra varchar(50)
+    sifra int not null primary key auto_increment,
+    lijecenje int not null,
+    medicinska_sestra int not null
 );
 
 create table lijecenje_pacijent(
-    sifra varchar(50),
-    lijecenje varchar(50),
-    pacijent varchar(50)
+    sifra int not null primary key auto_increment,
+    lijecenje int not null,
+    pacijent int not null
 );
+
+alter table doktor add foreign key (osoba) references osoba(sifra);
+alter table medicinska_sestra add foreign key (osoba) references osoba(sifra);
+alter table lijecenje add foreign key (doktor) references doktor(sifra);
+
+alter table lijecenje_sestra add foreign key (lijecenje) references lijecenje(sifra);
+alter table lijecenje_sestra add foreign key (medicinska_sestra) references medicinska_sestra(sifra);
+
+alter table lijecenje_pacijent add foreign key (lijecenje) references lijecenje(sifra);
+alter table lijecenje_pacijent add foreign key (pacijent) references osoba(sifra);
