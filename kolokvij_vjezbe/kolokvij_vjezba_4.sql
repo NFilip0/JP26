dedb4d28d8d2733d740a8cdcce313e9628eb154b
NFilip0 <filip.nad300@gmail.com>
2022-06-01 02:55:18 +0200

    peti i šesti zadatak

--- kolokvij_vjezbe/kolokvij_vjezba_4.sql
+++ kolokvij_vjezbe/kolokvij_vjezba_4.sql
@@ -124,3 +124,25 @@ delete from prijatelj where prstena > 17;
 # 4. Izlistajte haljina iz tablice snasa uz uvjet da vrijednost kolone treciputa nepoznate.
 
 select haljina from snasa where treciputa = null;
+
+# 5. Prikažite naušnica iz tablice mladic, jmbag iz tablice prijatelj te kratkamajica 
+# iz tablice becar uz uvjet da su vrijednosti kolone treciputa iz tablice snasa 
+# poznate te da su vrijednosti kolone lipa iz tablice zena različite od 29. 
+# Podatke posložite po kratkamajica iz tablice becar silazno.
+
+select a.nausnica ,f.jmbag ,e.kratkamajica 
+from mladic a
+inner join zena_mladic b on b.mladic =a.sifra 
+inner join zena c on c.sifra = b.zena 
+inner join snasa d on d.zena =c.sifra 
+inner join becar e on e.snasa =d.sifra 
+inner join prijatelj f on f.becar =e.sifra 
+where d.treciputa is not null and c.lipa!=29
+order by e.kratkamajica desc;
+
+# 6. Prikažite kolone lipa i prstena iz tablice zena čiji
+# se primarni ključ ne nalaze u tablici zena_mladic.
+select a.lipa ,a.prstena 
+from zena a
+left join zena_mladic b on b.zena =a.sifra
+where b.sifra is null;
