1eabcad0af6ec4712f5ce14de271da20bce52a27
NFilip0 <filip.nad300@gmail.com>
2022-06-07 21:49:34 +0200

    path,create

---
+++ kolokvij_vjezbe/kolokvij_vjezbe_7.sql
@@ -0,0 +1,77 @@
+# C:\xampp\mysql\bin\mysql -uroot --default_character_set=utf8 < C:\Users\filip\Documents\jp26\kolokvij_vjezbe\kolokvij_vjezba_7.sql
+
+drop database if exists kolokvij_vjezba_7;
+create database kolokvij_vjezba_7;
+
+use kolokvij_vjezba_7;
+
+# 0. Kreirajte tablice i veze između tablica.
+
+create table mladic(
+    sifra int not null primary key auto_increment,
+    prstena int,
+    lipa decimal(14,5) not null,
+    narukvica int not null,
+    drugiputa datetime not null
+);
+
+create table zarucnik_mladic(
+    sifra int not null primary key auto_increment,
+    zarucnik int not null,
+    mladic int not null
+);
+
+create table zarucnik(
+    sifra int not null primary key auto_increment,
+    vesta varchar(34),
+    asocijalno bit not null,
+    modelnaocala varchar(43),
+    narukvica int not null,
+    novcica decimal(15,5) not null
+);
+
+create table ostavljen(
+    sifra int not null primary key auto_increment,
+    lipa decimal(14,6),
+    introventno bit not null,
+    kratkamajica varchar(38) not null,
+    prstena int not null,
+    zarucnik int 
+); 
+
+create table prijateljica(
+    sifra int not null primary key auto_increment,
+    haljina varchar(45),
+    gustoca decimal(15,10) not null,
+    ogrlica int,
+    novcica decimal(12,5), 
+    ostavljen int
+);
+
+create table sestra(
+    sifra int not null primary key auto_increment,
+    bojakose varchar(34) not null,
+    hlace varchar(36) not null,
+    lipa decimal(15,6),
+    stilfrizura varchar(40) not null,
+    maraka decimal(12,8) not null,
+    prijateljica int
+);
+
+create table cura(
+    sifra int not null primary key auto_increment,
+    lipa decimal(12,9) not null,
+    introventno bit,
+    modelnaocala varchar(40),
+    narukvica int,
+    treciputa datetime,
+    kuna decimal(14,9)
+);
+
+create table punica(
+    sifra int not null primary key auto_increment,
+    majica varchar(40),
+    eura decimal(12,6) not null,
+    prstena int,
+    cura int not null
+);
