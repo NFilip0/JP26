e1b72e513f8e3792e1b169bfece24b51784f0814
NFilip0 <filip.nad300@gmail.com>
2022-06-27 20:57:17 +0200

    prvi zadatak

---
+++ java/eclipse/Edunova05/src/edunova/Zadatak01.java
@@ -0,0 +1,21 @@
+package edunova;
+
+import javax.swing.JOptionPane;
+
+public class Zadatak01 {
+
+	public static void main(String[] args) {
+		
+		// deklarirajte niz decimalnih
+		// brojeva dužine 12 i svim elementima
+		// niza postavite vrijednost 3,14
+		
+		double niz[] = new double[12];
+		int i = 0;
+		while(i<12) {
+			niz[i]=3.14;
+		}
+
+	}
+
+}
