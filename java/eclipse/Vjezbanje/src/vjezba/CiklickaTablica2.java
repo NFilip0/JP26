31ff5a0548d37d6dcd2d6d889ec34518f67c447c
NFilip0 <filip.nad300@gmail.com>
2022-07-09 07:39:43 +0200

    fix

--- java/eclipse/Vjezbanje/src/vjezba/CiklickaTablica2.java
+++ java/eclipse/Vjezbanje/src/vjezba/CiklickaTablica2.java
@@ -51,24 +51,24 @@ public class CiklickaTablica2 {
 		}/* rijesava problem kada je uneseno više stupaca nego redova
 		    bez ove petlje tablica 3x4 bi izgledala ovako
 		  		6   7   8   9
-   			5   0   0  10
-   			4   3   2   1  */
+   					5   0   0  10
+   					4   3   2   1  */
 		else if (r1 < r2 && c1 >= c2 && (y % 2 != 0)) {
 			for (int i = r2; i >= r1; i--) {
 				tablica[i][c1] = pb++;
 		}/* rijesava problem kada je uneseno više redaka nego stupaca
 			bez ove petlje tablica 4x3 bi izgledala ovako
 				6   7   8
-   			5   0   9
-   			4   0  10
-   			3   2   1*/
+   					5   0   9
+   					4   0  10
+   					3   2   1*/
 		}else if (x % 2 != 0 && (x == y)) {
 			tablica[r1][c1] = pb++;
 		}/* riješava problem kada je unesen isti broj redaka i stupaca
 			bez ove petlje tablica 3x3 bi izgledala ovako
 				5   6   7
-   			4   0   8
-   			3   2   1*/
+   					4   0   8
+   					3   2   1*/
 		
 		
 		for (int i = 0; i < x; i++) {
