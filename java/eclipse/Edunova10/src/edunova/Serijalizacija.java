042da753fadb05f07e9cd991fc68515737a777e0
NFilip0 <filip.nad300@gmail.com>
2022-07-14 00:44:22 +0200

    update

---
+++ java/eclipse/Edunova10/src/edunova/Serijalizacija.java
@@ -0,0 +1,34 @@
+package edunova;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.ObjectOutputStream;
+
+import edunova.model.Predavac;
+
+public class Serijalizacija {
+	
+	public Serijalizacija () {
+		
+		Predavac p = new Predavac();
+		p.setIme("Pero");
+		try {
+			FileOutputStream stream = new FileOutputStream(new File("datoteka.txt"));
+			ObjectOutputStream outputStream = new ObjectOutputStream(stream);
+			
+			outputStream.writeObject(p); // forsira implementaciju sučelja serializable
+			
+			outputStream.close();
+			stream.close();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public static void main(String[] args) {
+		new Serijalizacija();
+	}
+
+}
